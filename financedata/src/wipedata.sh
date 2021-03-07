264adfa7da23939786cf7ffd279e55756960bc04
freemanbach <flo@radford.edu>
2021-03-07 14:31:32 -0500

    automate delete files before git add

---
+++ financedata/src/wipedata.sh
@@ -0,0 +1,25 @@
+rm -f fb/*.csv
+rm -f goog/*.csv
+rm -f amzn/*.csv
+rm -f aapl/*.csv
+rm -f nflx/*.csv
+rm -f ibm/*.csv
+rm -f uber/*.csv
+rm -f jnj/*.csv
+rm -f mrna/*.csv
+rm -f pfe/*.csv
+rm -f orcl/*.csv
+rm -f bntx/*.csv
+
+mv fb_*.csv ./fb
+mv goog_*.csv ./goog
+mv amzn_*.csv ./amzn
+mv aapl_*.csv ./aapl
+mv nflx_*.csv ./nflx
+mv ibm_*.csv ./ibm
+mv uber_*.csv ./uber
+mv jnj_*.csv ./jnj
+mv mrna_*.csv ./mrna
+mv pfe_*.csv ./pfe
+mv orcl_*.csv ./orcl
+mv bntx_*.csv ./bntx
